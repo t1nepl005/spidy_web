02b47d9c0831191995679c71324607e576e452ba
Peter John <peterjohnhaboc05@gmail.com>
2025-09-03 15:45:09 +0800

    Added member: peter john

---
+++ resources/views/members/peterjohn.blade.php
@@ -0,0 +1,30 @@
+<x-member title="Christine Lopez Page" :wrapped="true"> 
+<div class="bio-container">
+    <img src="profilepic.jpg" alt="Profile Picture">
+    <h1>Peter John Haboc</h1>
+    <p>
+        Hi, I’m Peter John Haboc, a third-year IT student who enjoys learning new things and exploring how technology works. Outside of school, I like spending time in gaming, and hanging out with friends. I value being curious, creative, and open to new experiences, and I look forward to growing both personally and professionally in the future.
+    </p>
+
+    <div class="hobbies">
+        <h2>Hobbies</h2>
+        <ul>
+            <li>Watching Anime</li>
+            <li>Reading Comics</li>
+            <li>Playing Online Games</li>
+            <li>Playing Badminton</li>
+        </ul>
+    </div>
+
+    <div class="social-links">
+        <h2>Social</h2>
+        <a href="https://www.instagram.com/pjngz?igsh=MWgwY2x0a3dob3gwOA==" target="_blank">Instagram</a> |
+        <a href="https://www.facebook.com/share/14H2ReEhoru/" target="_blank">Facebook</a> 
+    </div>
+
+    <div class="contact-info">
+         Email: peterjohnhaboc15@gmail.com
+    </div>
+</div>
+
+</x-member>
